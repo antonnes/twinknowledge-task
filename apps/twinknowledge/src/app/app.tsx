03c67d27f0d136142c473a8662a3e04f078b23ac
Anthony Nikolov <tonnes.nk@gmail.com>
2025-04-23 13:08:35 +0300

    -reinitialise with nx-monrepo structure

---
+++ apps/twinknowledge/src/app/app.tsx
@@ -0,0 +1,20 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+
+
+
+    hello
+    
+      
+    </div>
+  );
+}
+
+export default App;
